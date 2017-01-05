8230ee25acae8cc4213ac6ca25fa18461e11a9dc
Hugi Romane <romane.h@codeur.online>
2017-01-05 14:50:49 +0100

    Modification vitesse

--- fichier.js
+++ fichier.js
@@ -1,7 +1,7 @@
 (function($){  
   setInterval(function(){  
-    $(".carrousel ul li:first-child").animate({"margin-left": -350}, 800, function(){  
+    $(".carrousel ul li:first-child").animate({"margin-left": -430}, 800, function(){  
         $(this).css("margin-left",0).appendTo(".carrousel ul");  
     });  
-  }, 3500);  
+  }, 2000);  
 })(jQuery);
