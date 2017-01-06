964d3e6517b8b6e2f19d6678a8ea776fcd13999d
Hugi Romane <romane.h@codeur.online>
2017-01-06 10:36:02 +0100

    Final !

--- fichier.js
+++ fichier.js
@@ -1,7 +1,30 @@
-(function($){  
-  setInterval(function(){  
-    $(".carrousel ul li:first-child").animate({"margin-left": -430}, 800, function(){  
-        $(this).css("margin-left",0).appendTo(".carrousel ul");  
+ 
+var interval = setInterval(function()
+{  
+    $("#carrousel ul").animate({"margin-left": -430}, function(){  
+        $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first")); 
     });  
   }, 2000);  
-})(jQuery);
+
+
+
+$(".next").click(function () 
+{
+    clearInterval(interval);
+    $("#carrousel ul").animate({marginLeft:-430},function()
+           {
+              $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+            })
+
+});
+
+
+$(".prev").click(function () 
+{
+    clearInterval(interval);
+    $("#carrousel ul").animate({marginRight:-430},function()
+           {
+              $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+            })
+
+});
